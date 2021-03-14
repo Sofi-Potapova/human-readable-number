d37e2e646bde74fcdc38355d8ea393020af9f82e
Potapova Sofia <ptv-sofia@yandex.by>
2021-03-14 18:27:02 +0300

    init: index.js

--- src/index.js
+++ src/index.js
@@ -1,42 +1,35 @@
 module.exports = function toReadable (number) {
-    let number = number.toString();
-    number = number.replace(/[\, ]/g,'');
-    if (number != parseFloat(number)) return 'not a number';
-    let x = number.indexOf('.');
-    if (x == -1)
-        x = number.length;
-    if (x > 15)
-        return 'too big';
-    let n = number.split(''); 
-    let str = '';
-    let sk = 0;
-    for (var i=0;   i < x;  i++) {
-        if ((x-i)%3==2) { 
-            if (n[i] == '1') {
-                str += tn[Number(n[i+1])] + ' ';
-                i++;
-                sk=1;
-            } else if (n[i]!=0) {
-                str += tw[n[i]-2] + ' ';
-                sk=1;
-            }
-        } else if (n[i]!=0) { 
-            str += dg[n[i]] +' ';
-            if ((x-i)%3==0) str += 'hundred ';
-            sk=1;
-        }
-        if ((x-i)%3==1) {
-            if (sk)
-                str += th[(x-i-1)/3] + ' ';
-            sk=0;
-        }
-    }
-    
-    if (x != number.length) {
-        var y = number.length;
-        str += 'point ';
-        for (var i=x+1; i<y; i++)
-            str += dg[n[i]] +' ';
-    }
-    return str.replace(/\s+/g,' ');
+    var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+'seventeen', 'eighteen', 'nineteen'];
+var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
+'ninety'];
+
+var numberString = number.toString();
+
+if (number < 0) throw new Error('Negative numbers are not supported.');
+
+if (number === 0) return 'zero';
+
+if (number < 20) {
+return ones[number];
+}
+
+if (numberString.length === 2) {
+return tens[numberString[0]] + ' ' + ones[numberString[1]];
+}
+
+if (numberString.length == 3) {
+if (numberString[1] === '0' && numberString[2] === '0')
+return ones[numberString[0]] + ' hundred';
+else
+return ones[numberString[0]] + ' hundred and ' + convert(+(numberString[1] + numberString[2]));
+}
+
+if (numberString.length === 4) {
+var end = +(numberString[1] + numberString[2] + numberString[3]);
+if (end === 0) return ones[numberString[0]] + ' thousand';
+if (end < 100) return ones[numSbertring[0]] + ' thousand and ' + convert(end);
+return ones[numberString[0]] + ' thousand ' + convert(end);
+}
 }
